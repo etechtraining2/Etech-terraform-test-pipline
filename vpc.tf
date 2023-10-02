7224745dba02662bc8caf399b7b9fc764423ecbb
etechengineer <terelenelvis89@gmail.com>
2023-10-02 14:34:55 -0400

    new env added

--- vpc.tf
+++ vpc.tf
@@ -6,8 +6,8 @@ resource "aws_vpc" "vpc" {
   enable_dns_hostnames = true
 
   tags = {
-    Name        = "etech VPC"
-    Environment = "sandbox"
+    Name        = "etech-stage VPC"
+    Environment = "stagging"
     Owner       = "Etechdevops"
   }
 }
