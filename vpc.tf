2b183cb74892e9658733fbe404d6070179a66b7b
Etech consulting <99599657+etechDevops@users.noreply.github.com>
2024-01-11 19:55:10 -0500

    vpc env updated (#45)
    Signed-off-by: etechengineer <terelenelvis89@gmail.com>
    Co-authored-by: etechengineer <terelenelvis89@gmail.com>

--- vpc.tf
+++ vpc.tf
@@ -6,8 +6,8 @@ resource "aws_vpc" "vpc" {
   enable_dns_hostnames = true
 
   tags = {
-    Name        = "team7-dev VPC"
-    Environment = "dev"
+    Name        = "team7-stage VPC"
+    Environment = "stage"
     Owner       = "Etechdevops"
     Team        = "team7"
   }
